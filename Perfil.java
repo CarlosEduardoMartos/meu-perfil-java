8ed2cee1fca7a855b7aad33f4a252e03b5baea6a
Carlos Eduardo Martos <59377606+CarlosEduardoMartos@users.noreply.github.com>
2025-02-24 21:29:51 -0300

    Commit exercicio aula 24/02

---
+++ Perfil.java
@@ -0,0 +1,7 @@
+public class HelloWord{
+    public static void main(String[]args){
+        System.out.println("Meu nome é Carlos Eduardo martos!");
+        System.out.println("tenho 19 anos");
+        System.out.println("E meu Hobby é assistir anime");
+    }
+}
